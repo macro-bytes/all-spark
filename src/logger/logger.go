b4bba025d9563f09249dd08e066539b0d0700b79
mshoaazar <mshoaazar@gmail.com>
2019-10-06 04:38:38 -1000

    (MS) Updated logging

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -3,19 +3,26 @@ package logger
 import (
 	"log"
 	"os"
+	"time"
 )
 
 // GetInfo - logs informational messages to STDOUT
 func GetInfo() *log.Logger {
-	return log.New(os.Stdout, "INFO: ", log.Lshortfile)
+	return log.New(os.Stdout,
+		time.Now().Format("2006-01-02 15:04:05")+" INFO: ",
+		log.Lshortfile)
 }
 
 // GetError - logs error message to STDERR
 func GetError() *log.Logger {
-	return log.New(os.Stderr, "ERROR: ", log.Lshortfile)
+	return log.New(os.Stderr,
+		time.Now().Format("2006-01-02 15:04:05")+" ERROR: ",
+		log.Lshortfile)
 }
 
 // GetFatal - logs fatal error message to STDERR and exits
 func GetFatal() *log.Logger {
-	return log.New(os.Stderr, "FATAL: ", log.Lshortfile)
+	return log.New(os.Stderr,
+		time.Now().Format("2006-01-02 15:04:05")+" FATAL: ",
+		log.Lshortfile)
 }
