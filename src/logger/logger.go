f6b6af6feee43dd6c44998ba8b070c90d594ed44
mshoaazar <mshoaazar@gmail.com>
2019-10-06 05:15:49 -1000

    (MS) Logging revision

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -9,20 +9,20 @@ import (
 // GetInfo - logs informational messages to STDOUT
 func GetInfo() *log.Logger {
 	return log.New(os.Stdout,
-		time.Now().Format("2006-01-02 15:04:05")+" INFO: ",
+		time.Now().Format("[2006-01-02 15:04:05]")+" INFO: ",
 		log.Lshortfile)
 }
 
 // GetError - logs error message to STDERR
 func GetError() *log.Logger {
 	return log.New(os.Stderr,
-		time.Now().Format("2006-01-02 15:04:05")+" ERROR: ",
+		time.Now().Format("[2006-01-02 15:04:05]")+" ERROR: ",
 		log.Lshortfile)
 }
 
 // GetFatal - logs fatal error message to STDERR and exits
 func GetFatal() *log.Logger {
 	return log.New(os.Stderr,
-		time.Now().Format("2006-01-02 15:04:05")+" FATAL: ",
+		time.Now().Format("[2006-01-02 15:04:05]")+" FATAL: ",
 		log.Lshortfile)
 }
